55e135d323aa47d45378319eb5bedabd11785df5
Florian Hammer <florianhammer@hotmail.de>
2018-03-12 22:01:47 +0100

    Added first charachter prototype
    - Added character controller
    - Added character motor
    - Added input script
    - Added personal dev scene
    - Added character prefab

---
+++ JustATestProject/Assets/CharacterMotor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterMotor : MonoBehaviour {
+    [SerializeField]
+    private float _MoveSpeed = 3;
+    [SerializeField]
+    private float _JumpForce = 250;
+
+    private Rigidbody2D myRigidbody;
+    private Vector2 velocity = new Vector2();
+    private Collider2D collider = null;
+
+    public bool IsOnGround
+    {
+        get
+        {
+            return Physics2D.Raycast(myRigidbody.position, Vector2.down, collider.bounds.size.y + 0.1f);
+        }
+    }
+
+	void Start ()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+	}
+
+    public void Move(float multiplier)
+    {
+        //prevents falling :(
+        Vector2 speed = Vector2.right * multiplier * Time.deltaTime * _MoveSpeed;
+
+        this.myRigidbody.MovePosition(myRigidbody.position + speed);
+    }
+
+    public void Jump(float multiplier)
+    {
+        this.myRigidbody.AddForce(Vector2.up * _JumpForce, ForceMode2D.Impulse);
+    }
+}
---
+++ JustATestProject/Assets/MyInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MyCharacterController))]
+public class MyInput : MonoBehaviour {
+    private MyCharacterController characterController;
+
+	void Start ()
+    {
+        characterController = GetComponent<MyCharacterController>();
+	}
+	
+	void Update ()
+    {
+        if(!Mathf.Approximately(Input.GetAxis("Horizontal"), 0))
+        {
+            characterController.Move(Input.GetAxis("Horizontal"));
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            characterController.Jump();
+        }
+	}
+}
